d1ff6b6acfe3f8d5bef16eee60d19d784a0a4823
Rohit Majila <rohitmajila345@hotmail.com>
2021-04-04 12:30:19 +0530

    Rever back to old

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Register} from './components/Auth/Register'
 
+function App(){
   return (
 
      <div>
-     
+     <Register/>
       </div>
     
   )
 
-
+  }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';  
 
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
 
-import reducers from './reducers/index'
 
-const store=createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={store}>
+
     <App />
-  </Provider>,
+ ,
   document.getElementById('root')
 );
 
